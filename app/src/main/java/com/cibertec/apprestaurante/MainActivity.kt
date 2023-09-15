2b637786dd174a921d6b3788111924373365b8e6
JC_Dev <juancarlosrresmiranda96@gmail.com>
2023-09-15 17:30:17 -0500

    :primer commit estado base del proyecto

---
+++ app/src/main/java/com/cibertec/apprestaurante/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cibertec.apprestaurante
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

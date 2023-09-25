80b0ef55f4d3efa3eb1d6d5d6efa8f072dd4914a
JC_dev <juancarlostorresmiranda96@gmail.com>
2023-09-25 00:52:00 -0500

    :seleccion de roles completa v3

---
+++ app/src/main/java/com/cibertec/apprestaurante/Activity/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.cibertec.apprestaurante.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.cibertec.apprestaurante.R
+
+class LoginActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
+        btnIngresar.setOnClickListener {
+            startActivity(Intent(this, RolActivity::class.java))
+        }
+
+
+        val btnRegistrar = findViewById<Button>(R.id.btnRegistrar)
+        btnRegistrar.setOnClickListener{
+            //startActivity(Intent(this, Lo::class.java))
+        }
+    }
+}
